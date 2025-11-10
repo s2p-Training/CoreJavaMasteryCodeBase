8db77b88b691b375049b2bbbffa3721c152d632f
pranayramteke <pranayramteke613@gmail.com>
2025-11-10 12:06:14 +0530

    Added Code For Quadratic Equation Root Calculation

--- src/section9_Operators_And_Expressions/Example11.java
+++ src/section9_Operators_And_Expressions/Example11.java
@@ -2,5 +2,18 @@ package section9_Operators_And_Expressions;
 
 public class Example11
 {
+    // Calculating The Root Of Quadratic Equations
+    public static void main(String[] args) {
 
+        int a = 1;
+        int b = 6;
+        int c = 9;
+
+        double delta = Math.pow(b,2) - (4 * a * c);
+        double root1 = (-b + Math.sqrt(delta)) / (2*a);
+        double root2 = (-b - Math.sqrt(delta)) / (2*a);
+
+        System.out.println("Root1 : " + root1);
+        System.out.println("Root2 : " + root2);
+    }
 }
