57f4c85244f32e8b4ab47f1dcc4e900a8a34da30
pranayramteke <pranayramteke613@gmail.com>
2025-12-30 11:09:24 +0530

    Added Code For Interfaces

--- src/section18_OOP_Interfaces/Example4.java
+++ src/section18_OOP_Interfaces/Example4.java
@@ -3,6 +3,7 @@ package section18_OOP_Interfaces;
 interface IMember
 {
     void callback();
+    boolean isPremiumMember();
 }
 
 class Store
@@ -20,7 +21,11 @@ class Store
     {
         for (int i=0; i < count; i++)
         {
-            members[i].callback();
+            // Notifying Only Premium Customers
+            if(true == members[i].isPremiumMember())
+            {
+                members[i].callback();
+            }
         }
     }
 
@@ -31,32 +36,42 @@ class Customer implements IMember
     String name;
     String email;
     String phoneNumber;
+    boolean isPremiumMember;
 
+    Customer(String name, String email, String phoneNumber, boolean isPremiumMember)
+    {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.isPremiumMember = isPremiumMember;
+    }
+
+    @Override
     public void callback()
     {
         System.out.println("Okay!, I Will Visit Store, " + name);
+    }
+
+    @Override
+    public boolean isPremiumMember()
+    {
+        return this.isPremiumMember;
     }
 }
 
 public class Example4
 {
     public static void main(String[] args) {
-        Customer customer1 = new Customer();
-        customer1.name = "Pranay";
-
-        Customer customer2 = new Customer();
-        customer2.name = "Rajesh";
-
-        Customer customer3 = new Customer();
-        customer3.name = "Rohan";
-
-        Customer customer4 = new Customer();
-        customer4.name = "Sachin";
+        Customer customer1 = new Customer("Prabhas", "prabhas@gmail.com", "7378736964",true);
+        Customer customer2 = new Customer("vijay", "thalapativijay@gmail.com","9423115052",false);
+        Customer customer3 = new Customer("pranay", "pranay@gmail.com","7874782378",true);
+        Customer customer4 = new Customer("john","john@gmail.com","9867542378",false);
 
         Store store = new Store();
 
         store.register(customer1);
         store.register(customer2);
+        store.register(customer3);
         store.register(customer4);
 
         store.notifyPremiumCustomers();
--- src/section18_OOP_Interfaces/Example5.java
+++ src/section18_OOP_Interfaces/Example5.java
@@ -1,17 +1,72 @@
 package section18_OOP_Interfaces;
 
+// Dos And Don'ts Of Interfaces
 interface Test1
 {
-    // by default methods are public abstract
+    // 1. by default methods in interface are public and abstract
     public abstract void method1();
     void method2();
+
+    // 2. Interface can not have private abstract methods
+//    private void method3();
+
+
+    // 3. Interfaces can have identifiers and these identifiers are static & final (Constant)
+    int X = 10;
+
+    // 4. Interface can have static concrete methods
+    public static void method3()
+    {
+        System.out.println("Static Method Present Inside Interface Test1");
+    }
+
+    // 6. Interface can have default method from java 8 onwards
+    default void method5()
+    {
+        System.out.println("Default Method-5");
+    }
+
+    // 7. Interface can have private concrete methods from java 9 onwards
+    // default method will use the private method
+    // private methods are useful only inside the interface
+    private void method6()
+    {
+        System.out.println("Private Method In Interface");
+    }
+}
+
+// 5. Interface can extend another interface
+interface Test2 extends Test1
+{
+    void method4();
+}
+
+class MyTestImpl implements Test2
+{
+    @Override
+    public void method1() {
+        System.out.println("Method-1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Method-2");
+    }
+
+    @Override
+    public void method4() {
+        System.out.println("Method-3");
+    }
+
 
 }
 
 
 public class Example5
 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        System.out.println(Test1.X);
+        Test1.method3();
     }
 }
